8abad3917b4a4087a35820d22c38e0d8e2bbe954
alifdoll <alifianfajarpratama@gmail.com>
2020-11-14 18:35:25 +0700

    Initial Commit

---
+++ Assets/Scripts/levelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+
+    int currentScene;
+    [SerializeField]int waitInSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene == 0)
+        {
+            StartCoroutine(Loading());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Loading()
+    {
+        yield return new WaitForSeconds(waitInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
